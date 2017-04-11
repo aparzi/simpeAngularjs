5c63ed0abf299553af78c08faefb8f0573c355aa
Angelo <angeloparziale94@gmail.com>
2017-04-11 17:11:55 +0200

    file updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
-var htmlPath = path.join(__dirname, 'pages')
+var htmlPath = path.join(__dirname, 'src')
 
 app.use(express.static(htmlPath))
 
